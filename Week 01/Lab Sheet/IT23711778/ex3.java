094826fd7574992a9b243fa4bdcc8350aa69a404
Manesha Dahanayake <maneshadahanayake03@gmail.com>
2025-06-17 15:02:27 +0530

    Add files via upload

---
+++ Week 01/Lab Sheet/IT23711778/ex3.java
@@ -0,0 +1,12 @@
+public class ex3{
+	public static void main(String args[]){
+		int age = 11;
+		
+		if(age>18){
+			System.out.println("Adult\n");
+		}
+		else{
+			System.out.println("Child\n");
+		}
+		}
+}
