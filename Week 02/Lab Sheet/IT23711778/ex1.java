b708a2c94271ae677c069db1726b44aeb17bf114
Manesha Dahanayake <maneshadahanayake03@gmail.com>
2025-06-18 21:18:54 +0530

    Update ex1.java

--- Week 02/Lab Sheet/IT23711778/ex1.java
+++ Week 02/Lab Sheet/IT23711778/ex1.java
@@ -1,1 +1,12 @@
+public class ex1 {
+    public static void main(String[] args) {
+        
+        int miles = 26;
+        int yards = 385;
+        double kilometers;
 
+        kilometers = miles * 1.609 + (yards / 1760.0) * 1.609;
+
+        System.out.println("The marathon distance in kilometers is: " + kilometers + " km");
+    }
+}
